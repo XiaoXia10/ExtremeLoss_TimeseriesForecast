eba528ae51b01408fd611dc9308cb263095ee3b6
XXL <130078715+XiaoXia10@users.noreply.github.com>
2025-03-17 12:29:42 -0400

    Add files via upload

---
+++ Graph_wavenet_yamaska/generate_training_well_data.py
@@ -0,0 +1,199 @@
+import argparse
+import numpy as np
+import os
+import pandas as pd
+from os.path import join
+from scipy import signal
+from data_scaler import standardize_df
+
+def _get_leap_years(year_list):
+    leaplist=[]
+    for year in year_list:
+        if ((year%4==0 and year%100!=0) or (year%400==0)):
+            leaplist.append(year)
+            
+    return leaplist
+
+
+def _generate_graph_seq2seq_io_data(df, seq_length_x, seq_length_y, shift, scaler=None):    
+    """
+    Generate samples from
+    :param df:
+    :param x_offsets: the x offsets are the x indices of the sequence.
+    e.g. if seq_length_x is 3 then x_offsets = [-2,-1,0]
+    
+    :param y_offsets: the y offsets are the y indices of the sequence.
+    e.g. if seq_length_y is 3 then x_offsets = [1, 2, 3]
+   
+    :return:
+    # x: (epoch_size, input_length, num_nodes, input_dim)
+    # y: (epoch_size, output_length, num_nodes, output_dim)
+    """
+    num_samples, num_nodes = df.shape
+    data = np.expand_dims(df, axis=-1)
+    feature_list = [data]
+    
+    df_time = df.index
+
+       
+    years = df.index.values.astype('datetime64[Y]').astype(int) + 1970
+    days = df.index.values.astype("datetime64[D]") - df.index.values.astype("datetime64[Y]")
+    days = days.astype("int32")+1
+
+    leap_years = _get_leap_years(np.unique(years))
+
+    scaled_days = [] 
+    
+    for day, year in zip(days, years):
+        if year in leap_years:
+            scaled_days.append(day/366)
+        else:
+            scaled_days.append(day/365)
+            
+    time_in_year = np.tile(scaled_days, [1, num_nodes, 1]).transpose((2, 1, 0))
+    feature_list.append(time_in_year)
+    
+    data = np.concatenate(feature_list, axis=-1)
+    x, y, time_list = [], [], []
+    #shift = seq_length_x
+    max_t = num_samples - (seq_length_y+shift)
+   
+    for t in range(0, max_t, shift):  # t is the index of the last observation.
+        
+        total_window_len = data[t:seq_length_x+seq_length_y+t]
+        x.append(total_window_len[:seq_length_x, ...])
+        y.append(total_window_len[seq_length_x:, ...]) 
+        
+        time_window_len = df_time[t:seq_length_x+seq_length_y+t]
+        time_list.append(time_window_len[seq_length_x:]) # Only collecting the time for y dataset
+        
+    x = np.stack(x, axis=0)
+    y = np.stack(y, axis=0)
+    time = np.stack(time_list, axis=0)
+    
+    
+    return x, y, time
+
+def generate_train_val_test(args):
+    """
+
+    Parameters
+    ----------
+    args : From parser
+       
+    Returns
+    -------
+    x_train : nummpy
+        Used for model training.
+    y_train : numpy
+        Used for model training.
+    x_val : numpy
+        Used for model validation.
+    y_val : numpy
+        Used for model validation.
+    x_test : numpy
+        Used for model testing.
+    y_test : numpy
+        Used for model testing.
+    """
+    
+    seq_length_x, seq_length_y, shift = args.seq_length_x, args.seq_length_y, args.shift
+    # df = pd.read_csv(args.df_train, index_col=0, parse_dates=True)
+    # df_test = pd.read_csv(args.df_test, index_col=0, parse_dates=True)
+    # x: (num_samples, input_length, num_nodes, input_dim)
+    # y: (num_samples, output_length, num_nodes, output_dim)
+    
+    x_offsets = np.sort(np.concatenate((np.arange(-(seq_length_x - 1), 1, 1),)))
+    # print('x offset ' + str(x_offsets))
+    # Predict the time steps
+    y_offsets = np.sort(np.arange(1, (seq_length_y + 1), 1))
+    # print('y offset ' + str(y_offsets))
+
+    print(x_offsets)
+    print(y_offsets)
+    
+    
+    if args.scale_data == True:
+        df = standardize_df(args.df_path, loader="train")
+        df_test = standardize_df(args.df_path, loader="test")
+    else:
+        df = pd.read_csv(args.df_train, index_col=0, parse_dates=True)
+        df_test = pd.read_csv(args.df_test, index_col=0, parse_dates=True)
+    
+    x, y, time = _generate_graph_seq2seq_io_data(
+        df,
+        seq_length_x,
+        seq_length_y,
+        shift
+    )
+
+    x_test, y_test, time_test = _generate_graph_seq2seq_io_data(
+        df_test,
+        seq_length_x,
+        seq_length_y,
+        shift
+    )    
+    
+    print("x shape: ", x.shape, ", y shape: ", y.shape)
+    print("x_test shape: ", x_test.shape, ", y_test shape: ", y_test.shape)
+    
+    # Write the data into npz file.
+    num_samples = x.shape[0]
+    num_train = round(num_samples * args.train_split)
+    num_val = num_samples - num_train
+    
+    x_train, y_train, time_train = x[:num_train], y[:num_train], time[:num_train]
+   
+    x_val, y_val,time_val = x[num_train:], y[num_train:], time[num_train:]
+    
+    # This is the data format for training and testing the Graph WaveNet model
+    for cat in ["train", "val", "test"]:
+        _x, _y = locals()["x_" + cat], locals()["y_" + cat]
+        print(cat, "x: ", _x.shape, "y:", _y.shape)
+        np.savez_compressed(
+            os.path.join(args.output_dir, f"{cat}.npz"),
+            x=_x,
+            y=_y,
+            x_offsets=x_offsets.reshape(list(x_offsets.shape) + [1]),
+            y_offsets=y_offsets.reshape(list(y_offsets.shape) + [1]),
+        )
+    
+    # Write data for reconstruction of data after model training 
+    # These files are for plotting and debugging
+    np.save(join( args.output_dir, "x_train.npy"), x_train)
+    np.save(join( args.output_dir, "y_train.npy"), y_train)
+    np.save(join( args.output_dir, "x_val.npy"), x_val)
+    np.save(join( args.output_dir, "y_val.npy"), y_val)
+    np.save(join( args.output_dir, "x_test.npy"), x_test)
+    np.save(join( args.output_dir, "y_test.npy"), y_test)
+    
+    np.save(join(args.output_dir, "train_time.npy"), time_train)
+    np.save(join(args.output_dir, "val_time.npy"), time_val)
+    np.save(join(args.output_dir, "test_time.npy"), time_test)
+    
+    print("The files are saved in the output directory")
+    return x_train, y_train, x_val, y_val, x_test, y_test
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    forecast = 5
+    parser.add_argument("--output_dir", type=str, default="data/GWN_"+str(forecast), help="Output directory.")
+    
+    parser.add_argument("--df_path", type=str, default="data", help="df path",)
+    # parser.add_argument("--time_dir", type=str, default="data_H/GWN_12", help="Time directory")
+    
+    parser.add_argument("--df_train", type=str, default="data/train_val_data.csv", help="df for train and validation.",)
+    parser.add_argument("--df_test", type=str, default="data/test_data.csv", help="df for testing.",)
+    parser.add_argument("--seq_length_x", type=int, default=forecast, help="X Sequence Length.",)
+    parser.add_argument("--seq_length_y", type=int, default=forecast, help="Y Sequence Length.",)
+    parser.add_argument("--shift", type=int, default=forecast, help="Default is seq_length_x", ) # this is a sequence window shift
+    parser.add_argument("--train_split", type=float, default=0.8, help="The percentage split for training and val.",)
+    parser.add_argument("--scale_data", type=bool, default=True, help="Data scaling",)
+
+    args = parser.parse_args()
+    if not os.path.exists(args.output_dir):
+        os.mkdir(args.output_dir)
+        
+    x_train, y_train, x_val, y_val, x_test, y_test = generate_train_val_test(args)
+    # generate_train_val_test(args)
+    
---
+++ Graph_wavenet_yamaska/util.py
@@ -0,0 +1,340 @@
+import argparse
+import pickle
+import numpy as np
+import os
+
+import pandas as pd
+import scipy.sparse as sp
+import torch
+from scipy.sparse import linalg
+
+DEFAULT_DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+class DataLoader(object):
+    def __init__(self, xs, ys, batch_size, pad_with_last_sample=True):
+        """
+        :param xs:
+        :param ys:
+        :param batch_size:
+        :param pad_with_last_sample: pad with the last sample to make number of samples divisible to batch_size.
+        """
+        self.batch_size = batch_size
+        self.current_ind = 0
+        if pad_with_last_sample:
+            num_padding = (batch_size - (len(xs) % batch_size)) % batch_size
+            x_padding = np.repeat(xs[-1:], num_padding, axis=0)
+            y_padding = np.repeat(ys[-1:], num_padding, axis=0)
+            xs = np.concatenate([xs, x_padding], axis=0)
+            ys = np.concatenate([ys, y_padding], axis=0)
+        self.size = len(xs)
+        self.num_batch = int(self.size // self.batch_size)
+        self.xs = xs
+        self.ys = ys
+
+    def shuffle(self):
+        permutation = np.random.permutation(self.size)
+        xs, ys = self.xs[permutation], self.ys[permutation]
+        self.xs = xs
+        self.ys = ys
+
+    def get_iterator(self):
+        self.current_ind = 0
+
+        def _wrapper():
+            while self.current_ind < self.num_batch:
+                start_ind = self.batch_size * self.current_ind
+                end_ind = min(self.size, self.batch_size * (self.current_ind + 1))
+                x_i = self.xs[start_ind: end_ind, ...]
+                y_i = self.ys[start_ind: end_ind, ...]
+                yield (x_i, y_i)
+                self.current_ind += 1
+
+        return _wrapper()
+
+
+class StandardScaler():
+
+    def __init__(self, mean, std, fill_zeroes=True):
+        self.mean = mean
+        self.std = std
+        self.fill_zeroes = fill_zeroes
+
+    def transform(self, data):
+        if self.fill_zeroes:
+            mask = (data == 0)
+            data[mask] = self.mean
+        return (data - self.mean) / self.std
+
+    def inverse_transform(self, data):
+        return (data * self.std) + self.mean
+
+
+
+def sym_adj(adj):
+    """Symmetrically normalize adjacency matrix."""
+    adj = sp.coo_matrix(adj)
+    rowsum = np.array(adj.sum(1))
+    d_inv_sqrt = np.power(rowsum, -0.5).flatten()
+    d_inv_sqrt[np.isinf(d_inv_sqrt)] = 0.
+    d_mat_inv_sqrt = sp.diags(d_inv_sqrt)
+    return adj.dot(d_mat_inv_sqrt).transpose().dot(d_mat_inv_sqrt).astype(np.float32).todense()
+
+def asym_adj(adj):
+    adj = sp.coo_matrix(adj)
+    rowsum = np.array(adj.sum(1)).flatten()
+    d_inv = np.power(rowsum, -1).flatten()
+    d_inv[np.isinf(d_inv)] = 0.
+    d_mat= sp.diags(d_inv)
+    return d_mat.dot(adj).astype(np.float32).todense()
+
+def calculate_normalized_laplacian(adj):
+    """
+    # L = D^-1/2 (D-A) D^-1/2 = I - D^-1/2 A D^-1/2
+    # D = diag(A 1)
+    :param adj:
+    :return:
+    """
+    adj = sp.coo_matrix(adj)
+    d = np.array(adj.sum(1))
+    d_inv_sqrt = np.power(d, -0.5).flatten()
+    d_inv_sqrt[np.isinf(d_inv_sqrt)] = 0.
+    d_mat_inv_sqrt = sp.diags(d_inv_sqrt)
+    normalized_laplacian = sp.eye(adj.shape[0]) - adj.dot(d_mat_inv_sqrt).transpose().dot(d_mat_inv_sqrt).tocoo()
+    return normalized_laplacian
+
+def calculate_scaled_laplacian(adj_mx, lambda_max=2, undirected=True):
+    if undirected:
+        adj_mx = np.maximum.reduce([adj_mx, adj_mx.T])
+    L = calculate_normalized_laplacian(adj_mx)
+    if lambda_max is None:
+        lambda_max, _ = linalg.eigsh(L, 1, which='LM')
+        lambda_max = lambda_max[0]
+    L = sp.csr_matrix(L)
+    M, _ = L.shape
+    I = sp.identity(M, format='csr', dtype=L.dtype)
+    L = (2 / lambda_max * L) - I
+    return L.astype(np.float32).todense()
+
+def load_pickle(pickle_file):
+    try:
+        with open(pickle_file, 'rb') as f:
+            pickle_data = pickle.load(f)
+    except UnicodeDecodeError as e:
+        with open(pickle_file, 'rb') as f:
+            pickle_data = pickle.load(f, encoding='latin1')
+    except Exception as e:
+        print('Unable to load data ', pickle_file, ':', e)
+        raise
+    return pickle_data
+
+
+def load_adj(file_path):
+    adj_mx = np.load(file_path)
+    return adj_mx
+
+
+def load_dataset(dataset_dir, batch_size, valid_batch_size=None, test_batch_size=None, n_obs=None, fill_zeroes=True):
+    data = {}
+    for category in ['train', 'val', 'test']:
+        cat_data = np.load(os.path.join(dataset_dir, category + '.npz'))
+        data['x_' + category] = cat_data['x']
+        data['y_' + category] = cat_data['y']
+        if n_obs is not None:
+            data['x_' + category] = data['x_' + category][:n_obs]
+            data['y_' + category] = data['y_' + category][:n_obs]
+    scaler = StandardScaler(mean=data['x_train'][..., 0].mean(), std=data['x_train'][..., 0].std(), fill_zeroes=fill_zeroes)
+    # Data format
+    for category in ['train', 'val', 'test']:
+        data['x_' + category][..., 0] = scaler.transform(data['x_' + category][..., 0])
+    data['train_loader'] = DataLoader(data['x_train'], data['y_train'], batch_size)
+    data['val_loader'] = DataLoader(data['x_val'], data['y_val'], valid_batch_size)
+    data['test_loader'] = DataLoader(data['x_test'], data['y_test'], test_batch_size)
+    data['scaler'] = scaler
+    return data
+
+
+def extreme_value_loss(y_true, y_pred, alpha=2.7):
+    """
+    Extreme Value Loss Function, focusing on extreme events in predictions in PyTorch.
+    
+    Parameters:
+    y_true (tensor): True values.
+    y_pred (tensor): Predicted values.
+    alpha (float): Coefficient for standard deviation to define extremes. This is a parameters that needs to be tuned
+    beta (float): Weight multiplier for extreme values.
+    
+    Returns:
+    loss (tensor): Computed loss value focusing on extreme events.
+    """
+    # Calculate mean and standard deviation based on true values
+    mean = torch.mean(y_true) # mean is 0 if data have been standarized
+    std_dev = torch.std(y_true) # std is 1 if data have been standarized
+    dat_min = torch.min(y_true)
+    dat_max = torch.max(y_true)
+    
+
+    beta = (dat_max-mean)/std_dev
+    gamma = torch.abs(mean-dat_min)/std_dev
+    
+    # Define extremes: values beyond 'alpha' standard deviations from the mean
+    extreme_mask_pos = torch.abs(y_true - mean) > alpha * std_dev
+    extreme_mask_neg = torch.abs(y_true - mean) < alpha * std_dev
+
+    
+    # Calculate absolute errors
+    errors = torch.abs(y_true - y_pred)
+
+    # Apply different weights to the errors based on whether they are extremes
+    weights= torch.where(extreme_mask_pos, beta * torch.ones_like(errors), torch.ones_like(errors))*torch.where(extreme_mask_neg, gamma * torch.ones_like(errors), torch.ones_like(errors))
+    
+    weighted_errors = weights * errors
+
+    # Mean error to form the losss
+    loss = torch.mean(weighted_errors)
+    return loss
+
+def calc_metrics_extreme(preds, labels, null_val=0.):
+    if np.isnan(null_val):
+        mask = ~torch.isnan(labels)
+    else:
+        mask = (labels != null_val)
+    mask = mask.float()
+    mask /= torch.mean(mask)
+    mask = torch.where(torch.isnan(mask), torch.zeros_like(mask), mask)
+    # print(preds.shape)
+    # print(labels.shape)
+    mse = (preds - labels) ** 2
+    mae = torch.abs(preds-labels)
+    mape = mae / labels
+    extreme_loss = extreme_value_loss(labels, preds)
+    
+    extreme_loss, mape, mse= [mask_and_fillna(l, mask) for l in [extreme_loss, mape, mse]]
+    rmse = torch.sqrt(mse)
+    return extreme_loss, mape, rmse
+
+    
+def calc_metrics(preds, labels, null_val=0.):
+    if np.isnan(null_val):
+        mask = ~torch.isnan(labels)
+    else:
+        mask = (labels != null_val)
+    mask = mask.float()
+    mask /= torch.mean(mask)
+    mask = torch.where(torch.isnan(mask), torch.zeros_like(mask), mask)
+    # print(preds.shape)
+    # print(labels.shape)
+    mse = (preds - labels) ** 2
+    mae = torch.abs(preds-labels)
+    mape = mae / labels
+    
+    mae, mape, mse = [mask_and_fillna(l, mask) for l in [mae, mape, mse]]
+    rmse = torch.sqrt(mse)
+    return mae, mape, rmse
+
+
+def mask_and_fillna(loss, mask):
+    loss = loss * mask
+    loss = torch.where(torch.isnan(loss), torch.zeros_like(loss), loss)
+    return torch.mean(loss)
+
+
+def calc_tstep_metrics(model, device, test_loader, scaler, realy, seq_length) -> pd.DataFrame:
+    model.eval()
+    outputs = []
+    for _, (x, __) in enumerate(test_loader.get_iterator()):
+        testx = torch.Tensor(x).to(device).transpose(1, 3)
+        with torch.no_grad():
+            preds = model(testx).transpose(1, 3)
+        outputs.append(preds.squeeze(1))
+    yhat = torch.cat(outputs, dim=0)[:realy.size(0), ...]
+    test_met = []
+
+    for i in range(seq_length):
+        pred = scaler.inverse_transform(yhat[:, :, i])
+        pred = torch.clamp(pred, min=0., max=70.)
+        real = realy[:, :, i]
+        test_met.append([x.item() for x in calc_metrics(pred, real)])
+    test_met_df = pd.DataFrame(test_met, columns=['mae', 'mape', 'rmse']).rename_axis('t')
+    
+    
+    return test_met_df, yhat
+
+
+def _to_ser(arr):
+    return pd.DataFrame(arr.cpu().detach().numpy()).stack().rename_axis(['obs', 'well num'])
+
+
+def make_pred_df_GWN(realy, yhat, scaler, seq_length):
+    df = pd.DataFrame(dict(y_last=_to_ser(realy[:, :, seq_length - 1]),
+                           yhat_last=_to_ser(scaler.inverse_transform(yhat[:, :, seq_length - 1])),
+                           y_3=_to_ser(realy[:, :, 2]),
+                           yhat_3=_to_ser(scaler.inverse_transform(yhat[:, :, 2]))))
+    return df
+
+def make_pred_df_wells(realy, yhat, scaler, seq_length, shift):
+    
+    if seq_length==shift:
+        print('shift length matches seq_length')
+        temp = torch.transpose(yhat, 1, 2 ).cpu().detach().numpy()
+        temp = temp.reshape(temp.shape[0]*temp.shape[1], temp.shape[2])
+        df_yhat = pd.DataFrame(scaler.inverse_transform(temp))
+    
+        temp = torch.transpose(realy, 1, 2 ).cpu().detach().numpy()
+        temp = temp.reshape(temp.shape[0]*temp.shape[1], temp.shape[2])
+        df_realy = pd.DataFrame(temp)
+        
+        return df_yhat, df_realy
+    
+    else:
+        print('shift length does not match seq_length')
+        realy = torch.transpose(realy, 1, 2 ).cpu().detach().numpy()
+        yhat = torch.transpose(yhat, 1, 2 ).cpu().detach().numpy()
+        yhat = scaler.inverse_transform(yhat)
+        
+        return yhat, realy
+
+def make_graph_inputs(args, device):
+
+    adj_mx = load_adj(args.adjdata)
+    supports = [torch.tensor(i).to(device) for i in adj_mx]
+    aptinit = None if args.randomadj else supports[0]  # ignored without do_graph_conv and add_apt_adj
+    if args.aptonly:
+        if not args.addaptadj and args.do_graph_conv: raise ValueError(
+            'WARNING: not using adjacency matrix')
+        supports = None
+    return aptinit, supports
+
+
+def get_shared_arg_parser(forecast = 5, loss = "extreme"):
+    
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('--device', type=str, default='cuda', help='')
+    parser.add_argument('--adjdata', type=str, default='adj_mat/adj_mat.npy', help='adj matrix path')
+    # parser.add_argument('--adjtype', type=str, default='doubletransition', help='adj type', choices=ADJ_CHOICES)
+    parser.add_argument('--do_graph_conv', action='store_true', help='whether to add graph convolution layer')
+    parser.add_argument('--aptonly', action='store_true', help='whether only adaptive adj')
+    parser.add_argument('--addaptadj', action='store_true', help='whether add adaptive adj')
+    parser.add_argument('--randomadj', action='store_true', help='whether random initialize adaptive adj')
+    
+    parser.add_argument('--data', type=str, default="data/GWN_"+str(forecast), help='data path')
+    parser.add_argument('--save', type=str, default="data/GWN_"+str(forecast)+"/experiment_"+str(forecast)+"_"+loss, help='save path')
+    parser.add_argument('--seq_length', type=int, default=forecast, help='') # need to change this
+    parser.add_argument("--shift", type=int, default=forecast, help="Default is seq_length_x", ) # this is the window shift
+    parser.add_argument('--nhid', type=int, default=forecast, help='Number of channels for internal conv') #need to be optimized
+    parser.add_argument('--in_dim', type=int, default=2, help='inputs dimension') # DONT touch this, this is the last dimension of the tensor which is fixed to be two.
+    parser.add_argument('--num_nodes', type=int, default=18, help='number of nodes')
+    parser.add_argument('--batch_size', type=int, default=32, help='batch size') 
+    parser.add_argument('--dropout', type=float, default=0.7, help='dropout rate')
+    
+    
+    parser.add_argument('--n_obs', default=None, help='Only use this many observations. For unit testing.')
+    parser.add_argument('--apt_size', default=10, type=int)
+    parser.add_argument('--cat_feat_gc', action='store_true')
+    parser.add_argument('--fill_zeroes', action='store_true')
+    parser.add_argument('--checkpoint', type=str, help='Create checkpoints for model training')
+    
+    return parser
+
+
+
